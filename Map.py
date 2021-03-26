3eda060e0749fb8047bca3828916e6b259bdd2bc
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-03-26 15:19:50 -0700

    working on self.grid in Map

--- Map.py
+++ Map.py
@@ -7,12 +7,16 @@ class Map:
     RED = ( 255, 0, 0)
 
     def __init__(self, width, height):
-        self.grid = []
         self.height = height
         self.width = width
+        self.grid = []
         pygame.init()
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Dungeon Generator with Pygame")
         
-        for w in width:
-            self.grid.append([None] * (self.height - 1) )
+        for w in range(self.width // 2):
+            temp = []
+            for h in range(self.height // 2):
+                temp.append(pygame.Rect((2 * w, 2 * h), (2,2)))
+                # pygame.draw.rect(self.screen, Map.RED, self.grid[w][h])
+            self.grid.append(temp)

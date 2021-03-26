b99f51ade89e78b7df5b9da4c3a0de8c9cb6e137
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-03-26 14:51:49 -0700

    pygame stuff added

--- Map.py
+++ Map.py
@@ -1,4 +1,13 @@
+import pygame 
+
 class Map:
-    __init__(self, width, height):
+    BLACK = ( 0, 0, 0)
+    WHITE = ( 255, 255, 255)
+    GREEN = ( 0, 255, 0)
+    RED = ( 255, 0, 0)
+
+    def __init__(self, width, height):
         self.height = height
-        self.width = width 
+        self.width = width
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.width, self.height))

c8a85a3988f3b5607f7ca7662da84b7bfb69ea45
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-04-02 14:55:55 -0700

    added the Tile class

--- Main.py
+++ Main.py
@@ -1,33 +1,31 @@
 import pygame
 from Map import Map
 
-running = True 
-map = Map (400, 400)
+running = True
+mapMange = Map(600, 600)
 
 clock = pygame.time.Clock()
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            ruunning = False
+            running = False
     WIDTH = 20
     HEIGHT = 20
-    
+
     # This sets the margin between each cell
     MARGIN = 5
 
-    pygame.draw.rect(map.screen,
-                             Map.RED,
-                             [(MARGIN + WIDTH) * 10 + MARGIN,
-                              (MARGIN + HEIGHT) * 10 + MARGIN,
-                              WIDTH,
-                              HEIGHT])
- 
+    pygame.draw.rect(mapMange.screen,
+                     Map.RED,
+                     [(MARGIN + WIDTH) * 10 + MARGIN,
+                      (MARGIN + HEIGHT) * 10 + MARGIN,
+                      WIDTH,
+                      HEIGHT])
+
     # map.screen.fill(Map.BLACK)
 
-    
     clock.tick(60)
     pygame.display.flip()
-
 
 pygame.quit()
